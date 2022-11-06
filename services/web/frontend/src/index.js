92587dfcc269428c6ea1a581514c14ec55ab2da3
Joep van Dijk <joep.van.dijk04@gmail.com>
2022-11-06 18:15:50 +0100

    Added react-app and router

---
+++ services/web/frontend/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import Root from "./views/Root";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {  
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
